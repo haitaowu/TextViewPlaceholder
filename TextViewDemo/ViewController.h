a406debec7beda0743e9f2ee60db202d567b013f
haitaowu <taotaowoo@gmail.com>
2016-06-06 09:17:32 +0800

    Initial Commit

---
+++ TextViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextViewDemo
+//
+//  Created by taotao on 6/6/16.
+//  Copyright © 2016 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextViewDemo
+//
+//  Created by taotao on 6/6/16.
+//  Copyright © 2016 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
